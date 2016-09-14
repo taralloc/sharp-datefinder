fa4898485563eeea9723ee6ea82ae2f4da67329e
Francesco <13621287+taralloc@users.noreply.github.com>
2016-09-14 22:21:04 +0200

    Tests
    Addes tests

---
+++ src/Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sharp_datefinder;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("test.txt");
+            string text = reader.ReadToEnd();
+            reader.Close();
+
+            DateFinder engine = new DateFinder();
+            List<DateFinderResult> dates = engine.ExtractDates(text);
+            
+            foreach(DateFinderResult res in dates)
+            {
+                Console.WriteLine("{0} | day was set {1} | year was set {2}", res.Date.ToShortDateString(), res.IsDaySet, res.IsYearSet);
+            }
+
+            Console.Read();
+        }
+    }
+}
